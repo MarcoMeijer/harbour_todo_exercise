6ee10e72a60c60ee5d869ba743c3af7d65449bf6
Marco Meijer <marcomeijerm@gmail.com>
2023-03-21 20:49:15 +0100

    feat: deleting items from list

--- src/components/MyLists.tsx
+++ src/components/MyLists.tsx
@@ -5,6 +5,9 @@ import classNames from 'classnames';
 import { CreateList } from '@/components/CreateList';
 import { randomColor } from '@/utils/randomColor';
 import { useState } from 'react';
+import { gql } from 'graphql-request';
+import { client } from '@/lib/client';
+import { Close } from './icons/Close';
 
 export type TodoList = {
   id: number;
@@ -17,6 +20,12 @@ type MyListsProps = {
   list: TodoList[];
 };
 
+const DELETE_LIST_MUTATION = gql`
+  mutation Mutation($deleteListId: Int!) {
+    deleteList(id: $deleteListId)
+  }
+`;
+
 export const MyLists = ({ list = [] }: MyListsProps) => {
   const [todoLists, setTodoLists] = useState<TodoList[]>(list);
 
@@ -24,26 +33,37 @@ export const MyLists = ({ list = [] }: MyListsProps) => {
     setTodoLists([...todoLists, newTodoList]);
   };
 
-  const onDeletedHandler = (id: string) => {
-    // TODO: delete list with query
-    // Update state with new list
+  const onDeletedHandler = async (deleteListId: number) => {
+    await client.request(DELETE_LIST_MUTATION, {
+      deleteListId,
+    });
+
+    setTodoLists(todoLists.filter((list) => list.id !== deleteListId));
   };
 
   return (
     <div className="flex flex-col gap-8 text-center">
-      <h1 className="text-4xl">{todoLists.length > 0 ? 'My TODO lists' : 'No lists yet!'}</h1>
+      <h1 className="text-4xl">
+        {todoLists.length > 0 ? 'My TODO lists' : 'No lists yet!'}
+      </h1>
       <ul>
         {todoLists.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} className="flex row ">
             <Link
               href={item.id.toString()}
               className={classNames(
-                'py-2 pl-4 pr-2 bg-gray-900 rounded-lg mb-4 flex justify-between items-center min-h-16 text-black hover:scale-[1.02] transform transition duration-300 ease-in-out',
+                'py-2 pl-4 pr-2 bg-gray-900 rounded-lg mb-4 flex flex-1 justify-between items-center min-h-16 text-black hover:scale-[1.02] transform transition duration-300 ease-in-out',
                 randomColor(),
               )}
             >
               {item.name}
             </Link>
+            <button
+              className="btn btn-square btn-error"
+              onClick={() => onDeletedHandler(item.id)}
+            >
+              <Close />
+            </button>
           </li>
         ))}
       </ul>
